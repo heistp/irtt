1f44735cf1d5685e1c98802c77096d5e8414947d
Pete Heist <peteheist@users.noreply.github.com>
2017-10-15 11:34:33 +0200

    Work on README.md. Add a few more platforms to build.sh.

--- build.sh
+++ build.sh
@@ -24,19 +24,21 @@ for a in $*; do
 		;;
 		"prod") tags="$tags prod"
 		;;
-		"openbsd-amd64") env="GOOS=openbsd GOARCH=amd64"
+		"linux-386"|"linux") env="GOOS=linux GOARCH=386"
 		;;
-		"freebsd-amd64") env="GOOS=freebsd GOARCH=amd64"
-		;;
-		"linux-amd64") env="GOOS=linux GOARCH=amd64"
+		"linux-amd64"|"linux64") env="GOOS=linux GOARCH=amd64"
 		;;
 		"linux-arm"|"rpi") env="GOOS=linux GOARCH=arm"
+		;;
+		"linux-mips64"|"erl") env="GOOS=linux GOARCH=mips"
+		;;
+		"linux-mipsle"|"erx"|"om2p") env="GOOS=linux GOARCH=mipsle"
+		;;
+		"darwin-amd64"|"osx") env="GOOS=darwin GOARCH=amd64"
 		;;
 		"win"|"windows") env="GOOS=windows GOARCH=386"
 		;;
 		"win64"|"windows64") env="GOOS=windows GOARCH=amd64"
-		;;
-		"linux-mipsle"|"erx"|"om2p") env="GOOS=linux GOARCH=mipsle"
 		;;
 		*) echo "Unknown parameter: $a"
 		exit 1
