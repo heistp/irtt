f51143a23b688c346950f5c130f87fe9c9be2718
Pete Heist <peteheist@users.noreply.github.com>
2017-11-21 20:54:44 +0100

    Improve build date info.

--- version.go
+++ version.go
@@ -1,4 +1,7 @@
 package irtt
 
-// Version is the version number.
+// Version is the version number (replaced during build).
 var Version string
+
+// BuildDate is the build date (replaced during build).
+var BuildDate string
